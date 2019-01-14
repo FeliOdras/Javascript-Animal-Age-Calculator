f8a261a4cbb17b77e54392acfa37aaab83d9da23
FeliOdras <ariane.feldmann@odras.de>
2019-01-14 15:38:43 +0100

    Created 3 different outputs

--- assets/js/animalAgeCalculator.js
+++ assets/js/animalAgeCalculator.js
@@ -7,9 +7,13 @@ function calculateAge(animal, humanAge) {
     } else if (animal = 'chincilla') {
         animalAge = humanAge * 25;
     }
-    console.log(`${humanAge} years are ${animalAge} years in a ${animal}'s life`);
+    let calcResultOutput = `${humanAge} years are ${animalAge} years in a ${animal}'s life`;
+    document.getElementById('animalAgeResult').innerHTML = calcResultOutput;
+
 }
-calculateAge('dog', 6)
+calculateAge('dog', 12);
+calculateAge('cow', 4);
+calculateAge('chinchilla', 2);
 
 
 // document.getElementById('animalAgeResult').innerHTML = calulateAge();
