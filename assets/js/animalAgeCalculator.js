0b48956602556f2d6a8aa902e40bc1a3da8e05af
FeliOdras <ariane.feldmann@odras.de>
2019-01-15 09:14:47 +0100

    Get values from form input - output is generated but does not stay.

--- assets/js/animalAgeCalculator.js
+++ assets/js/animalAgeCalculator.js
@@ -1,10 +1,10 @@
-function calculateAge(animal, humanAge) {
+/*function calculateAge(animal, humanAge) {
     let animalAge;
     if (animal = 'dog') {
-        animalAge = humanAge * 7;
-    } else if (animal = 'cow') {
+        animalAge == humanAge * 7;
+    } else if (animal == 'cow') {
         animalAge = humanAge * 14;
-    } else if (animal = 'chincilla') {
+    } else if (animal == 'chinchilla') {
         animalAge = humanAge * 25;
     }
     let calcResultOutput = `${humanAge} years are ${animalAge} years in a ${animal}'s life`;
@@ -13,7 +13,22 @@ function calculateAge(animal, humanAge) {
 }
 calculateAge('dog', 12);
 calculateAge('cow', 4);
-calculateAge('chinchilla', 2);
-
+calculateAge('chinchilla', 2);*/
+function calc() {
+    let animal = document.getElementById('animalType').value;
+    let humanAge = document.getElementById('humanAge').value;
+    console.log(animal);
+    console.log(humanAge);
+    let animalAge;
+    if (animal == 'dog') {
+        animalAge = humanAge * 7;
+    } else if (animal == 'cow') {
+        animalAge = humanAge * 14;
+    } else if (animal == 'chinchilla') {
+        animalAge = humanAge * 25;
+    }
+    console.log(animalAge);
+    document.getElementById('animalAgeResult').innerHTML = `${humanAge} years are ${animalAge} years in a ${animal}'s life`;
+}
 
 // document.getElementById('animalAgeResult').innerHTML = calulateAge();
